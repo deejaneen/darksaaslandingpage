8b71573c0e50ae6c66aed2b66882b6c1b89ded51
deeja-ub <tomeldendjaninetara@gmail.com>
2024-08-19 22:13:03 +0800

    Implement other components and animations to 3 components

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
+import helixImage from '../assets/images/helix2.png'
+import emojiStarImage from '../assets/images/emojistar.png'
 
 const CallToAction = () => {
   return (
-    <div>
+    <div className='bg-black text-white py-[72px] sm:py-24 text-center'>
+      <div className='container max-w-xl relative'>
+        <Image src={helixImage} alt='helix image' className='absolute top-6 left-[calc(100%+36px)] hidden sm:inline-flex' height={200} width={200}/>
+        <Image src={emojiStarImage} alt='emoji star' className='absolute -top-[120px] right-[calc(100%+24px)]' height={200} width={200}/>
+        <h2 className='font-bold text-5xl tracking-tighter sm:text-6xl'>
+          Get instant access
+        </h2>
+        <p className='text-xl text-white/70 mt-5'>
+        Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
+        </p>
+        <form className='mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row'>
+          <input type="email" placeholder='your@email.com'className='h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1'/>
+          <button className='bg-white text-black h-12 rounded-lg px-5 min-w-max'>Get access</button>
+        </form>
+      </div>
       
     </div>
   )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,15 @@
+"use client";
 import React from "react";
 import ArrowWIcon from "../assets/icons/arrow-w.svg";
 import cursorImage from "../assets/images/cursor.png";
 import messageImage from "../assets/images/message.png";
 import Image from "next/image";
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
-      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] "></div>
+      <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] lg:h-[300px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)] "></div>
       <div className="container relative">
         <div className="flex items-center justify-center">
           <a
@@ -28,8 +30,26 @@ const Hero = () => {
             <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center  inline-flex">
               One Task <br /> at a Time
             </h1>
-            <Image src={cursorImage} alt="cursor image" height={200} width={200} className="absolute right-[476px] top-[108px] hidden sm:inline"/>
-            <Image src={messageImage} alt="message image" height={200} width={200} className="absolute left-[498px] top-[56px] hidden sm:inline"/>
+            <motion.div className="absolute right-[476px] top-[108px] hidden sm:inline" drag dragSnapToOrigin>
+              <Image
+                src={cursorImage}
+                alt="cursor image"
+                height={200}
+                width={200}
+                className="max-w-none"
+                draggable="false"
+              />
+            </motion.div>
+            <motion.div className="absolute left-[498px] top-[56px] hidden sm:inline" drag dragSnapToOrigin>
+            <Image
+              src={messageImage}
+              alt="message image"
+              height={200}
+              width={200}
+              className="max-w-none"
+              draggable="false"
+            />
+            </motion.div>
           </div>
         </div>
         <div className="flex justify-center">
